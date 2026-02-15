5f6a68d7895765fadd1dafb9dad36eed349f4ef8
pisething <112937826+pisething@users.noreply.github.com>
2026-02-15 10:47:27 +0700

    first commit

---
+++ src/main/java/com/piseth/java/school/ownerservice/OwnerserviceApplication.java
@@ -0,0 +1,13 @@
+package com.piseth.java.school.ownerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnerserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnerserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piseth/java/school/ownerservice/OwnerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piseth.java.school.ownerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnerserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
